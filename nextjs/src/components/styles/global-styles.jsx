aac36c17798ea8d466784fe6357186980521e098
Deetechcomputers <cartadaniel01@gmail.com>
2026-05-19 02:19:27 +0000

    Enlarge mobile product preview image

--- nextjs/src/components/styles/global-styles.jsx
+++ nextjs/src/components/styles/global-styles.jsx
@@ -12120,7 +12120,7 @@ button, input, select, textarea { font: inherit; }
   }
   .product-preview__stage img {
     width: 100%;
-    max-height: calc(100dvh - 220px);
+    max-height: calc(100dvh - 180px);
   }
   .product-preview__thumbs {
     grid-template-columns: 28px minmax(0, 1fr) 28px;
