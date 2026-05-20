f6c91bf58af1eada0d22f777f3ffb2e9c0b0c362
Deetechcomputers <cartadaniel01@gmail.com>
2026-05-20 04:30:48 +0000

    Fix favicon crop visibility

--- nextjs/src/app/layout.js
+++ nextjs/src/app/layout.js
@@ -14,7 +14,7 @@ const defaultOgImage = {
   height: 630,
   alt: "Deetech Computers",
 };
-const siteIconHref = "/logo-favicon.png?v=deetech-20260520c";
+const siteIconHref = "/logo-favicon.png?v=deetech-20260520d";
 
 export const metadata = {
   metadataBase: new URL(SITE_URL),
